150e960f870fe8d877c3061dabb8e319b80229c9
Nicolas Siver <nicolas@siver.im>
2015-10-03 00:42:27 -0400

    populate progress

--- public/js/overview.js
+++ public/js/overview.js
@@ -10,7 +10,7 @@ define('forum/client/points/overview', ['components', 'forum/points/ranking'], f
 
     Overview.init = function () {
         var container = document.getElementsByClassName('points-users')[0];
-        var i, len = ajaxify.data.users.length, payload, htmlRow, htmlUser, rankMeta, progress;
+        var i, len = ajaxify.data.users.length, payload, htmlRow, htmlUser, rankMeta;
 
         for (i = 0; i < len; ++i) {
             payload = ajaxify.data.users[i];
@@ -25,38 +25,19 @@ define('forum/client/points/overview', ['components', 'forum/points/ranking'], f
                 basePoints: ajaxify.data.pointsSettings.basePoints,
                 baseGrow  : ajaxify.data.pointsSettings.baseGrow
             }, payload.points);
-            progress = rankMeta.rankProgress / rankMeta.rankTotal * 100;
 
             //Inject additional parameters
             payload.relative_path = ajaxify.data.relative_path;
             payload.rank = rankMeta.rank;
             payload.rankProgress = getProgressMessage(rankMeta);
+            //TODO Add Stugger animation for progress bars
+            payload.progress = rankMeta.rankProgress / rankMeta.rankTotal * 100;
 
             htmlUser = document.createElement('div');
             htmlUser.className = className;
             htmlUser.innerHTML = templates.parse(ajaxify.data.userTemplate, payload);
             htmlRow.appendChild(htmlUser);
         }
-
-        //$('[component="points/top-user"]').each(function (index, userDomElement) {
-        //    var element     = $(this),
-        //        points      = element.data('points'),
-        //        basePoints  = ajaxify.data.pointsSettings.basePoints,
-        //        baseGrow    = ajaxify.data.pointsSettings.baseGrow,
-        //        rankMeta    = ranking.defaultRanking({
-        //            basePoints: basePoints,
-        //            baseGrow  : baseGrow
-        //        }, points),
-        //        progressBar = element.find('.progress-bar'),
-        //        progress    = rankMeta.rankProgress / rankMeta.rankTotal * 100;
-        //
-        //    element.find('.rank').text(rankMeta.rank);
-        //    element.find('.rank-progress').text(getProgressMessage(rankMeta));
-        //
-        //    //TODO Add Stugger animation for progress bars
-        //    progressBar.data('valuenow', progress);
-        //    progressBar.css('width', progress + '%');
-        //});
     };
 
     function getProgressMessage(rankMeta) {
