92a2e03d460f34d79585687a8d26a831195099bd
Nicolas Siver <nicolas@siver.im>
2016-04-25 23:41:27 -0400

    setup initial structure for acp client

---
+++ client/acp/webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry       : "./index.js",
+    output      : {
+        path         : "../../public/js",
+        filename     : "acp.js",
+        libraryTarget: "amd",
+        library      : "admin/plugins/points"
+    },
+    module      : {
+        loaders: [
+            {
+                test   : /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader : 'babel-loader',
+                query  : {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    watchOptions: {
+        poll: 1000
+    },
+    plugins     : [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ]
+};
