5e4afdd715e34316f6982c2dcb9cf4414efc5b13
Nicolas Siver <nicolas@siver.im>
2018-05-21 20:45:03 -0400

    add benchpress dependency

--- public/js/overview.js
+++ public/js/overview.js
@@ -2,7 +2,7 @@
 
 /* globals define, app, ajaxify, bootbox, socket, templates, utils */
 
-define('forum/client/points/overview', ['components', 'forum/points/ranking'], function (components, ranking) {
+define('forum/client/points/overview', ['forum/points/ranking', 'benchpress'], function (ranking, Benchpress) {
 
     var Overview  = {},
         columns   = 4,
@@ -10,40 +10,65 @@ define('forum/client/points/overview', ['components', 'forum/points/ranking'], f
         delay     = 0.1;
 
     Overview.init = function () {
-        var container = document.getElementsByClassName('points-users')[0];
-        var i, len = ajaxify.data.users.length, payload, htmlRow, htmlUser, rankMeta;
+        var users = ajaxify.data.users;
 
-        for (i = 0; i < len; ++i) {
-            payload = ajaxify.data.users[i];
+        if (users.length > 0) {
+            Benchpress.registerLoader(function (name) {
+                if (name === 'ns-points') {
+                    return Promise.resolve(function () {
+                        return ajaxify.data.userTemplate;
+                    });
+                }
+            });
 
-            if (i % columns == 0) {
-                htmlRow = document.createElement('div');
-                htmlRow.className = 'row';
-                container.appendChild(htmlRow);
-            }
-
-            //Calculation
-            rankMeta = ranking.defaultRanking({
-                basePoints: ajaxify.data.pointsSettings.basePoints,
-                baseGrow  : ajaxify.data.pointsSettings.baseGrow
-            }, payload.points);
-
-            //Inject additional parameters
-            payload.relative_path = ajaxify.data.relative_path;
-            payload.rank = rankMeta.rank;
-            payload.rankProgress = getProgressMessage(rankMeta);
-            payload.progress = rankMeta.rankProgress / rankMeta.rankTotal * 100;
-
-            htmlUser = document.createElement('div');
-            htmlUser.className = className;
-            htmlUser.style['animation-delay'] = delay * i + 's';
-            htmlUser.innerHTML = templates.parse(ajaxify.data.userTemplate, payload);
-            htmlRow.appendChild(htmlUser);
+            renderUser(
+                document.getElementsByClassName('points-users')[0], null, 0, users
+            );
         }
     };
 
     function getProgressMessage(rankMeta) {
         return rankMeta.rankProgress + ' / ' + rankMeta.rankTotal;
+    }
+
+    function renderUser(container, row, cursor, users) {
+        var htmlRow, htmlUser, rankMeta;
+        var payload = users[cursor];
+
+        if (cursor % columns === 0) {
+            htmlRow = document.createElement('div');
+            htmlRow.className = 'row';
+            container.appendChild(htmlRow);
+        } else {
+            htmlRow = row;
+        }
+
+        //Calculation
+        rankMeta = ranking.defaultRanking({
+            basePoints: ajaxify.data.pointsSettings.basePoints,
+            baseGrow  : ajaxify.data.pointsSettings.baseGrow
+        }, payload.points);
+
+        //Inject additional parameters
+        payload.relative_path = ajaxify.data.relative_path;
+        payload.rank = rankMeta.rank;
+        payload.rankProgress = getProgressMessage(rankMeta);
+        payload.progress = rankMeta.rankProgress / rankMeta.rankTotal * 100;
+
+        htmlUser = document.createElement('div');
+        htmlUser.className = className;
+        htmlUser.style['animation-delay'] = delay * cursor + 's';
+
+        Benchpress
+            .render('ns-points', payload)
+            .then(function (content) {
+                htmlUser.innerHTML = content;
+                htmlRow.appendChild(htmlUser);
+
+                if (cursor + 1 < users.length) {
+                    renderUser(container, htmlRow, cursor + 1, users);
+                }
+            });
     }
 
     return Overview;
